7cc5ff4e73b536b9c10fc03cf2ba3d83f15e8a18
Benoît Legat <benoit.legat@gmail.com>
2017-05-22 22:02:36 +0200

    Add basicpolyhedrontests

--- test/polyhedron.jl
+++ test/polyhedron.jl
@@ -1,6 +1,7 @@
 const polyhedra_test = joinpath(Pkg.dir("Polyhedra"), "test")
 
 include(joinpath(polyhedra_test, "alltests.jl"))
-@testset "Polyhedra tests" for arith in [:float, :exact]
+@testset "Polyhedra tests with $arith arithmetic" for arith in [:float, :exact]
+    basicpolyhedrontests(CDDLibrary(arith))
     runtests(CDDLibrary(arith))
 end
