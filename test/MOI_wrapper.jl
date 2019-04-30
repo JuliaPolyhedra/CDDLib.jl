7ed848097a1f51502e3e6ad6a9684025602cd41b
Benoît Legat <benoit.legat@gmail.com>
2019-04-30 10:18:57 +0200

    Rework @testset's

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -7,7 +7,7 @@ const MOIB = MOI.Bridges
 using Polyhedra
 using CDDLib
 
-@testset "Continuous Linear problems with CDDLib.Optimizer" begin
+@testset "MOI wrapper" begin
     @testset "coefficient_type $T" for T in [Rational{BigInt}, Float64]
         @test Polyhedra.coefficient_type(CDDLib.Optimizer{T}()) == T
     end
@@ -15,11 +15,13 @@ using CDDLib
     @testset "SolverName" begin
         @test MOI.get(optimizer, MOI.SolverName()) == "CDD"
     end
-    cache = MOIU.UniversalFallback(Polyhedra._MOIModel{Float64}())
-    cached = MOIU.CachingOptimizer(cache, optimizer)
-    bridged = MOIB.full_bridge_optimizer(cached, Float64)
-    config = MOIT.TestConfig(duals=false)
-    MOIT.contlineartest(bridged, config,
-                        # linear8a and linear12 will be solved by https://github.com/JuliaOpt/MathOptInterface.jl/pull/702
-                        ["linear8a", "linear12", "partial_start"])
+    @testset "Continuous Linear problems with CDDLib.Optimizer" begin
+        cache = MOIU.UniversalFallback(Polyhedra._MOIModel{Float64}())
+        cached = MOIU.CachingOptimizer(cache, optimizer)
+        bridged = MOIB.full_bridge_optimizer(cached, Float64)
+        config = MOIT.TestConfig(duals=false)
+        MOIT.contlineartest(bridged, config,
+                            # linear8a and linear12 will be solved by https://github.com/JuliaOpt/MathOptInterface.jl/pull/702
+                            ["linear8a", "linear12", "partial_start"])
+    end
 end
