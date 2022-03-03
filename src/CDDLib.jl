311d93786bab37a52bcc92c7193218742d1dcc42
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2022-03-03 07:23:43 -0500

    Remove BinaryProvider (#70)

--- src/CDDLib.jl
+++ src/CDDLib.jl
@@ -1,30 +1,13 @@
 module CDDLib
 
 using LinearAlgebra
-
-if VERSION < v"1.3"
-    if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
-        include("../deps/deps.jl")
-    else
-        error("CDDLib not properly installed. Please run Pkg.build(\"CDDLib\")")
-    end
-else
-    import cddlib_jll: libcddgmp
-end
-
+import cddlib_jll: libcddgmp
 using Polyhedra
 
 include("ccall.jl")
 
-@static if VERSION < v"1.3"
-    function __init__()
-        check_deps()
-        @dd_ccall set_global_constants Nothing ()
-    end
-else
-    function __init__()
-        @dd_ccall set_global_constants Nothing ()
-    end
+function __init__()
+    @dd_ccall set_global_constants Nothing ()
 end
 
 import Base.convert, Base.push!, Base.eltype, Base.copy
