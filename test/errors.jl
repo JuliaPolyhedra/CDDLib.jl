43a4253aced9ce6d4d7299dd0013737af74f20d6
Benoît Legat <benoit.legat@gmail.com>
2025-04-29 18:51:45 +0200

    Add tests for myerror

--- test/errors.jl
+++ test/errors.jl
@@ -8,3 +8,12 @@ end
 @testset "Infinite $x" for x in [1 // 0, -2 // 0, NaN, Inf, -Inf]
     _invalid(x)
 end
+
+@testset "myerror" begin
+    err = ErrorException("A : Dimension too large")
+    @test_throws err CDDLib.myerror("A", CDDLib.Cdd_ErrorType(0))
+    err = ErrorException("A gave an error code of 18 which is out of the range of known error code. Pleasre report this by opening an issue at https://github.com/JuliaPolyhedra/CDDLib.jl.")
+    @test_throws err CDDLib.myerror("A", CDDLib.Cdd_ErrorType(18))
+    err = ErrorException("A gave an error code of -1 which is out of the range of known error code. Pleasre report this by opening an issue at https://github.com/JuliaPolyhedra/CDDLib.jl.")
+    @test_throws err CDDLib.myerror("A", CDDLib.Cdd_ErrorType(-1))
+end
