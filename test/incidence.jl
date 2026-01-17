4d1b9f9c07329ce7f999f0d582d7982115627e86
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2026-01-17 21:25:33 +0900

    Correct test logic

--- test/incidence.jl
+++ test/incidence.jl
@@ -18,14 +18,15 @@ using CDDLib
 
     @testset "Input incidence $precision" for precision in [:float, :exact]
         V = [[1//2, 1//2], [0, 1], [0, 0]]
-        incidence_extected = Set([
-            BitSet([1, 2]),
-            BitSet([1, 3]),
-            BitSet([2, 3])
-        ])
         p = polyhedron(vrep(V), CDDLib.Library(precision))
-        @test p isa CDDLib.Polyhedron{precision == :float ? Float64 : Rational{BigInt}}
+        T = Polyhedra.coefficient_type(p)
         hrep(p)
-        @test Set(copyinputincidence(p.poly)) == incidence_extected
+        incidence_computed = copyinputincidence(p.poly)
+        for v in eachindex(points(p))
+            for i in incidence_computed[v.value]
+                h = Polyhedra.Index{T, HalfSpace{T, Vector{T}}}(i)
+                @test Polyhedra.isincident(p, v, h; tol=0)
+            end
+        end
     end
 end
